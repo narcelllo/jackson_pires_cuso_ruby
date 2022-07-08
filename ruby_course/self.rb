134bcb2c358e089c3bcf39b2883f0ce1ec48a900
Marcello Magno <marcello_magno@hotmail.com>
2022-07-08 13:27:01 -0300

    self

---
+++ ruby_course/self.rb
@@ -0,0 +1,14 @@
+class Person
+    def speak
+        "Hello guys!"
+    end
+
+    def my_id
+        "my id: #{self.object_id}"
+end     
+
+p = Person.new
+puts p.meu_id
+
+p2 = Person.new
+puts p2.meu_id
